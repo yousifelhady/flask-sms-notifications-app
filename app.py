8330feeffbb9dc649e7da0ce0808b87e7b673a00
yousifelhady <yousif.abdelhamed@gmail.com>
2021-02-24 20:14:35 +0200

    refactoring endpoints of sending sms and sending notifications, implementing their corresponding unit tests and covering db insertions in tests

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_cors import CORS
 import os
-import pathlib
 from werkzeug.exceptions import HTTPException
 from datetime import datetime
 import re
@@ -56,14 +55,16 @@ def send_sms():
     
     decorated_message = decorate_message(message, client)
     send_sms_to_contact(contact, subject, decorated_message)
-    store_message_in_db(subject, message, client.id)
+    new_message_id = store_message_in_db(subject, message, client.id)
     #return frontend expected JSON
     return jsonify({
         'success': True,
         'contact': contact,
-        'message': message
+        'message': message,
+        'message_id': new_message_id
     }), 200
 
+# Validate the passed contact with respect to its country code and its format
 def is_valid_contact_format(client_contact):
     # check that the contact starts with '+20' which is Egypt's country code 
     # and end with number
@@ -75,8 +76,10 @@ def is_valid_contact_format(client_contact):
     is_valid_contact_len = len(client_contact) == contact_fixed_length
     return is_valid_format and is_valid_contact and is_valid_contact_len
 
+# Decorate the sent message before sending it
+# Note: It is not necessary and can be eliminated if it is not required to have
 def decorate_message(message, client):
-    # applying some message decoration (if required)
+    # applying some message decoration
     client_name = client.name
     if not client_name:
         client_name = ""
@@ -97,79 +100,91 @@ def store_message_in_db(subject, message, client_id):
     current_time = datetime.now()
     new_message = Message(subject=subject, body=message, time=current_time, client_id=client_id)
     new_message.insert()
+    return new_message.id
 
 @app.route('/send-notification', methods=['POST'])
 def send_notification():
-    push_service = FCMNotification(api_key=api_key)
     body = request.get_json()
-    registration_ids = body.get('tokens')
+    tokens = body.get('tokens')
     notification_title = body.get('title')
     notification_body = body.get('body')
-    
-    result = send_notification_to_devices(push_service, registration_ids, notification_title, notification_body)
+
+    # Check that passed tokens exist in database
+    # Such that the api should send notifications to only subscribed tokens
+    filtered_tokens = filter_tokens(tokens)
+    result = send_notification_to_devices(filtered_tokens, notification_title, notification_body)
     print(result)
+
     if isinstance(result, list):
         success = bool(result[0].get('success'))
     else:
         success = bool(result['success'])
     if success:
-        # The following database action could be eliminated as the API is not responsible for db actions
+        # The following database action could be eliminated as the API should not be responsible for db actions
         # Explanation:
-        # "handle_database_actions" function stores the passed tokens and the sent notification in database
-        # and it store the relation between them in third table (as their relation is Many to Many)
-        # Another alternatives:
-        # 1. use firestore db to store tokens and sent notifications
-        # 2. use mongodb as nosql db
-        # 3. log the notifications (sender, targeted token, title and body) in a log file for tracking and debugging purposes
-        handle_database_actions(notification_title, notification_body, registration_ids)
+        # "store_notification" function stores the sent notification in database
+        # and it creates a relation between the sent notifications and the tokens (as their relation is Many to Many)
+        # Another alternative:
+        # log the notifications (sender, targeted token, title and body) in a log file for tracking and debugging purposes
+        notification_id = store_notification(notification_title, notification_body, filtered_tokens)
+    
     return jsonify({
         'success': success,
-        'tokens': registration_ids,
+        'tokens': filtered_tokens,
         'title': notification_title,
-        'body': notification_body
+        'body': notification_body,
+        'notification_id': notification_id
     }), 200
 
-def send_notification_to_devices(push_service, registration_ids, notification_title, notification_body):
-    if isinstance(registration_ids, list):
-        # if passed registration ids list is empty, raise exception with status code: 400 Bad Request
-        if registration_ids == []:
-            raise RegistrationIDsNULLException(status_code=400)
-        return push_service.notify_multiple_devices(registration_ids=registration_ids, message_body=notification_body, message_title=notification_title)
-    else:
-        return push_service.notify_single_device(registration_id=registration_ids, message_body=notification_body, message_title=notification_title)
+# A method that filters passed tokens with the tokens existing in the database
+# it returns filtered tokens which are the ones that exist in database 
+# and discard the ones that does not exist in database
+def filter_tokens(tokens):
+    all_existing_tokens_in_db = Token.query.all()
+    all_existing_tokens = [_token.token for _token in all_existing_tokens_in_db]
+    filtered_tokens = []
+    for token in tokens:
+        if token in all_existing_tokens:
+            filtered_tokens.append(token)
+    return filtered_tokens
 
-def handle_database_actions(title, body, registration_ids):
+def send_notification_to_devices(tokens, notification_title, notification_body):
+    push_service = FCMNotification(api_key=api_key)
+    if isinstance(tokens, list):
+        # if passed registration ids list is empty, raise exception with status code: 400 Bad Request
+        if tokens == []:
+            raise RegistrationIDsNULLException(status_code=400)
+        return push_service.notify_multiple_devices(registration_ids=tokens, message_body=notification_body, message_title=notification_title)
+    else:
+        return push_service.notify_single_device(registration_id=tokens, message_body=notification_body, message_title=notification_title)
+
+def store_notification(title, body, tokens):
     notification_id = store_notification_in_db(title, body)
-    store_tokens_notification_relation_in_db(registration_ids, notification_id)
+    store_tokens_notification_relation_in_db(tokens, notification_id)
+    return notification_id
 
 def store_notification_in_db(title, body):
     current_time = datetime.now()
     newNotification = Notification(title=title, body=body, time=current_time)
     newNotification.insert()
-    newNotification_id = newNotification.id
-    return newNotification_id
-    
-def store_tokens_notification_relation_in_db(registration_ids, notification_id):
-    stored_tokens = Token.query.all()
-    stored_tokens = [_token.token for _token in stored_tokens]
-    for reg_id in registration_ids:
-        if reg_id not in stored_tokens:
-            newToken = Token(token=reg_id)
-            newToken.insert()
-            token_id = newToken.id
-        else:
-            existing_token = Token.query.filter_by(token=reg_id).first()
-            token_id = existing_token.id
-        token_notification_entry = TokenNotification(token_id=token_id, notification_id=notification_id)
-        token_notification_entry.insert()
+    return newNotification.id
+
+# Store a relation between the sent notification id and targeted tokens ids
+# So the history of notifications and their recipients are maintained 
+def store_tokens_notification_relation_in_db(tokens, notification_id):
+    for token in tokens:
+        token_obj = Token.query.filter_by(token=token).first()
+        if token_obj is not None:
+            token_notification_entry = TokenNotification(token_id=token_obj.id, notification_id=notification_id)
+            token_notification_entry.insert()
 
 @app.route('/notify-topic', methods=['POST'])
 def notify_topic():
-    push_service = FCMNotification(api_key=api_key)
     body = request.get_json()
     topic_name = body.get('topic')
     message_body = body.get('body')
     message_title = body.get('title')
+    push_service = FCMNotification(api_key=api_key)
     result = push_service.notify_topic_subscribers(topic_name=topic_name, message_body=message_body, message_title=message_title)
     print(result)
     success = bool(result['success'])
@@ -178,7 +193,10 @@ def notify_topic():
     else:
         status_code = 500
     return jsonify({
-        'success': success
+        'success': success,
+        'topic': topic_name,
+        'title': message_title,
+        'body': message_body
     }), status_code
 
 @app.errorhandler(HTTPException)
--- test_app.py
+++ test_app.py
@@ -2,7 +2,8 @@ import os
 import unittest
 import json
 
-from app import app
+from app import app, is_valid_contact_format
+from models import Message, Notification
 
 class TestApp(unittest.TestCase):
     def setUp(self):
@@ -12,12 +13,28 @@ class TestApp(unittest.TestCase):
         self.send_sms_json = {
             'contact': "+201009129288",
             'subject': 'testSubject',
-            'message': 'testMessage'
+            'message': 'test message body'
         }
-
+        self.send_topic_json = {
+            'topic': "news",
+            'title': 'test topic title',
+            'body': 'test topic message body'
+        }
+        self.send_notification_json = {
+            'tokens': [
+                "dTWVMWRcwXARbMwcMBrz9V:APA91bHiaBYFTK_toYgFto3kNdlE9TaoJPPhEMSu1b0Yob20QH8D9j2RWQTalFgmJPv9oiCac-DBnexkd-rlsVhqGbcH0vqwZVPW2Q3tok1xQ56o9OJFUnBRaAjIHredgQkNZODi8xU2",
+                "dyimeAKczeP3UJ8ynvI1I2:APA91bHQFAK2d28Tyfg89zqWVrPynCCEXF9eNnRW705fFxEdDE4klEBsqlVsdWiXl3jkWykCQ503Nh4m6EeL3tNS7iR1mnCB9e_Q7Sw_wDd_N3nENiqwmpTV2e1blahBck03zhR9t4LJ"
+            ],
+            'title': "test notification title",
+            'body': "test notification message body"
+        }
+        self.invalid_contact = "01009129288"
+        self.valid_contact = "+201009129288"
     def tearDown(self):
         pass
     
+    # Testing Endpoints
+    ###################
     def test_index(self):
         res = self.client().get('/')
         self.assertEqual(res.status_code, 200)
@@ -25,20 +42,57 @@ class TestApp(unittest.TestCase):
     def test_send_sms(self):
         res = self.client().post('/send-sms', json=self.send_sms_json)
         res_data = json.loads(res.data)
-
+        sent_message = Message.query.order_by(Message.id.desc()).first()
+        
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res_data['success'], True)
         self.assertEqual(res_data['contact'], self.send_sms_json['contact'])
+        self.assertTrue(res_data['message'])
+        self.assertEqual(res_data['message_id'], sent_message.id)
 
-    # def test_send_sms_limit(self):
-    #     i = 4
-    #     while i > 0:
-    #         res = self.client().post('/send-sms', json=self.send_sms_json)
-    #         i -= 1
-    #     res_data = json.loads(res.data)
+    def test_send_sms_limit(self):
+        i = 4
+        while i > 0:
+            res = self.client().post('/send-sms', json=self.send_sms_json)
+            i -= 1
+        res_data = json.loads(res.data)
 
-    #     self.assertEqual(res.status_code, 429)
-    #     self.assertEqual(res_data['success'], False)
+        self.assertEqual(res.status_code, 429)
+        self.assertEqual(res_data['success'], False)
+
+    def test_notify_topic(self):
+        res = self.client().post('/notify-topic', json=self.send_topic_json)
+        res_data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertEqual(res_data['topic'], self.send_topic_json['topic'])
+        self.assertTrue(res_data['title'])
+        self.assertTrue(res_data['body'])
+
+    def test_send_notification(self):
+        res = self.client().post('/send-notification', json=self.send_notification_json)
+        res_data = json.loads(res.data)
+        sent_notification = Notification.query.order_by(Notification.id.desc()).first()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertEqual(res_data['tokens'], self.send_notification_json['tokens'])
+        self.assertTrue(res_data['title'])
+        self.assertTrue(res_data['body'])
+        self.assertEqual(res_data['notification_id'], sent_notification.id)
+
+    # Testing Helper functions
+    ##########################
+    def test_is_valid_contact_format_with_invalid_contact(self):
+        tested_value = is_valid_contact_format(self.invalid_contact)
+        expected_value = False
+        self.assertEqual(expected_value, tested_value)
+
+    def test_is_valid_contact_format_with_valid_contact(self):
+        tested_value = is_valid_contact_format(self.valid_contact)
+        expected_value = True
+        self.assertEqual(expected_value, tested_value)
 
 if __name__ == "__main__":
     unittest.main()
